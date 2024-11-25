bcd665cbc87d8e2d6df8a66e47a5cb41856eedc8
Hugo Messias <hugomessias9@gmail.com>
2024-11-25 19:07:18 -0300

    Commit inicial - Criadas funções para fazer pesquisas, coletar informações de filmes e séries e fazer downloads no site 'warezcdn.link' - Implementados argumentos de linha de comando para fazer pesquisas e downloads - Criadas funções para baixar arquivos m3u8

---
+++ utils.py
@@ -0,0 +1,119 @@
+# TODO: add support for mixdrop downloads
+# TODO: create docstrings
+import requests
+
+from tempfile import TemporaryDirectory
+import subprocess
+import time
+import os
+import re
+
+from downloader import Download
+
+
+def download_m3u8(url: str, output_file: str):
+    response = requests.get(url)
+    with open(output_file, 'bw') as file:
+        file.write(response.content)
+
+
+# TODO: continue download if failed
+#   change 'download_from_m3u8' path from a temp dir to combination of file id and audio id
+#   skip parts already present on the parts directory
+#   delete path after finished
+def download_parts(m3u8_path: str, headers: dict = None, output_dir: str = './', max_downloads: int = 1):
+    # fromat arguments
+    if output_dir[-1] not in ('/', '\\'):
+        output_dir += '/'
+    
+    if headers is None:
+        headers = {}
+    
+    # create directory for parts
+    parts_dir = f"{output_dir}parts/"
+    os.makedirs(parts_dir)
+
+    # download every url inside the playlist
+    with open(m3u8_path, 'r', encoding='utf8') as file:
+        playlist = file.readlines()
+        line_count = len(playlist)
+        curr_line = 0
+        print('0.00%')
+
+        for line in playlist:
+            curr_line += 1
+            line = line.strip()
+            url = re.match(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", line)
+            if url:
+                url = url[0]
+                file_name = f"{parts_dir}{re.findall(r".*/(.+)\.", url)[0]}.mp4"
+
+                try:
+                    Download(url, file_name, headers=headers, max_retries=5, try_continue=False).start()
+
+                except ValueError:
+                    continue
+                
+                while Download.get_running_count() >= max_downloads:
+                    time.sleep(0.01)
+            
+            print(f"\033[F\r{curr_line/(line_count/100):.2f}%")
+        
+    Download.wait_downloads(False, 5)
+
+    return parts_dir
+
+
+# TODO: make it ignore invalid links, such as links with unsatisfiable ranges
+def create_local_m3u8(m3u8_path: str, parts_dir: str):
+    # fromat arguments
+    if parts_dir[-1] not in ('/', '\\'):
+        parts_dir += '/'
+    
+    full_parts_dir = os.path.abspath(parts_dir) + '/'
+
+    # create function to properly order the files inside the given directory
+    def extract_number(filename):
+        match = re.search(r'(\d+)', filename)
+        return int(match.group(0)) if match else 0
+
+    # get files list and order them
+    parts = [file for file in os.listdir(parts_dir) if file.rsplit('.', 1)[-1] == 'mp4']
+    parts = sorted(parts, key=extract_number)
+
+    # create local m3u8 file
+    local_m3u8_path = f'{parts_dir}local.m3u8'
+    with open(m3u8_path, 'r') as playlist:
+        with open(local_m3u8_path, 'w') as output:
+            for line in playlist:
+                line = line.strip()
+                if re.match(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", line):
+                    output.write(f"{full_parts_dir}{parts.pop(0)}\n")
+                
+                else:
+                    output.write(f"{line}\n")
+
+    return local_m3u8_path
+
+
+# TODO: implement ability to pass custom arguments to ffmpeg
+def concat(local_m3u8_path: str, output_file: str):
+    # run ffmpeg to concatenate files
+    print(os.getcwd())
+    subprocess.run(["ffmpeg",
+                    "-i", local_m3u8_path, 
+                    "-c", "copy",
+                    f"{output_file}"
+                    ])
+    
+
+# TODO: change to default temp folder
+# TODO: retry if download fails
+def download_from_m3u8(url: str, output_file: str):
+    with TemporaryDirectory(dir='') as path:
+        m3u8_path = f"{path}/index.m3u8"
+        download_m3u8(url, m3u8_path)
+
+        parts_dir = download_parts(m3u8_path, output_dir=path, max_downloads=5)
+        local_m3u8_path = create_local_m3u8(m3u8_path, parts_dir)
+        concat(local_m3u8_path, output_file)
---
+++ warezcdn.py
@@ -0,0 +1,345 @@
+# TODO: create unit tests
+# TODO: implement a 'search and download' function
+# TODO: create docstrings
+# TODO: show steps of the process to get the download link
+# TODO: give better names to the output files from episodes
+import argparse
+import typing
+import json
+import re
+
+from bs4 import BeautifulSoup
+import requests
+
+from utils import download_from_m3u8
+
+
+host = 'warezcdn.link'
+host_url = f'https://embed.{host}'
+
+
+def search(search_term: str):
+    response = requests.post(
+        f'{host_url}/includes/ajax.php', 
+        headers={'Host': host}, 
+        data={'searchBar': search_term},
+        allow_redirects=False
+        )
+    
+    return response.json()
+
+
+def serie(imdb: str):
+    #  get referer url
+    referer_url = f'{host_url}/serie/{imdb}'
+    referer_response = requests.get(referer_url)
+
+    # extract url for getting the series info from the acquired html
+    html = BeautifulSoup(referer_response.content, 'html.parser')
+    scripts = html.find_all('script')
+    for script in scripts:
+        serie_url = re.findall(r"var cachedSeasons = (?:\'|\")(.+)(?:\'|\")", script.string)
+        if serie_url:
+            serie_url = f'{host_url}/{serie_url[0]}'
+            break
+    
+    # request series info and return json response
+    serie = search(imdb)['list']['0']
+    seasons = requests.get(serie_url, headers={'Referer': referer_url})
+
+    serie_info = serie | seasons.json()
+
+    return serie_info
+
+
+def filme(imdb: str):
+    return search(imdb)['list']['0']
+
+
+def get_audios(imdb: str, id: str, type: typing.Literal['movie', 'filme', 'serie']):
+    if type == 'movie':
+        type = 'filme'
+
+    referer_url = f'{host_url}/{type}/{imdb}'
+
+    match type:
+        case 'serie':
+            # request audio data
+            request_url = f'{host_url}/core/ajax.php?audios={id}'
+
+            response = requests.get(
+                request_url,
+                headers={'Referer': referer_url}
+                )
+    
+            return json.loads(response.json())
+        
+        case 'filme':
+            # get referer html
+            response = requests.get(referer_url)
+
+            # extract audio data from the referer html
+            html = BeautifulSoup(response.content, 'html.parser')
+            scripts = html.find_all('script')
+            for script in scripts:
+                audio_data = re.findall(r"let data = (?:\'|\")(.+)(?:\'|\")", script.string)
+                if audio_data:
+                    audio_data = audio_data[0]
+                    break
+
+            return json.loads(audio_data)
+
+
+def get_parts_url(
+        imdb: str,
+        id: str, 
+        server: typing.Literal['warezcdn', 'mixdrop'], 
+        lang: typing.Literal['1', '2'], 
+        type: typing.Literal['movie', 'filme', 'serie']
+    ):
+    if type == 'movie':
+        type = 'filme'
+
+    referer_url = f'{host_url}/{type}/{imdb}'
+    embed_referer_url = f'{host_url}/getEmbed.php?id={id}&sv={server}&lang={lang}'
+    play_url = f'{host_url}/getPlay.php?id={id}&sv={server}'
+
+    # get referer and embed to avoid bot detection
+    requests.get(referer_url)
+    requests.get(
+        embed_referer_url,
+        headers={'Referer': referer_url}
+        )
+    
+    # get embed play html
+    play_response = requests.get(
+        play_url,
+        headers={'Referer': embed_referer_url}
+        )
+    
+    # extract url to the video player from play_html
+    play_html = BeautifulSoup(play_response.content, 'html.parser')
+    scripts = play_html.find_all('script')
+    for script in scripts:
+        video_html_url = re.findall(r"window.location.href = (?:\'|\")(.+)(?:\'|\")", script.string)
+        if video_html_url:
+            video_html_url = video_html_url[0]
+            break
+
+    # extract information about the video from video_html_url
+    video_host = re.findall(r"https://(.+?)/", video_html_url)[0]
+    video_host_url = f'https://{video_host}'
+    video_hash = re.findall(r"/([\w]+?)(?:$|\?)", video_html_url)[0]
+
+    # make request for master.m3u8 url based on data from video_html_url
+    master_request_url = f'{video_host_url}/player/index.php?data={video_hash}&do=getVideo'
+
+    master_m3u8_url = requests.post(
+        master_request_url,
+        data={'hash': video_hash, 'r': ''},
+        headers={'X-Requested-With': 'XMLHttpRequest'}
+        )
+    
+    master_m3u8_url = master_m3u8_url.json()['videoSource']
+
+    # extract the url for the playlist containing all the parts from master.m3u8
+    master_m3u8 = requests.get(master_m3u8_url).text
+    for line in master_m3u8.split('\n'):
+        matches = re.match(r"https?://[a-zA-Z0-9.-]+(?:\.[a-zA-Z]{2,})(:\d+)?(/[^\s]*)?", line)
+        if matches:
+            playlist_url = matches[0]
+            break
+        
+    return playlist_url
+
+
+# TODO: warn if prefered audio is unavaliable
+def download_episode(ep_name: str, imdb: str, id: str, preferred_audio: typing.Literal['1', '2']):
+    audios = get_audios(imdb, id, 'serie')
+    for audio in audios:
+        if audio['audio'] == preferred_audio:
+            break
+
+    if 'warezcdn' in audio['servers']:
+        server = 'warezcdn'
+    else:
+        server = 'mixdrop'
+    
+    if server == 'warezcdn':
+        parts_url = get_parts_url(imdb, audio['id'], server, audio['audio'], 'serie')
+        
+        download_from_m3u8(parts_url, f'{ep_name}.mp4')
+    
+    else:
+        msg = "O episódio possui apenas o servido 'mixdrop', que ainda não é suportado."
+        raise Exception(msg)
+
+
+def download_serie(
+        imdb: str, 
+        season: int, 
+        episodes: int | list[int] | typing.Literal['all'], 
+        preferred_audio: typing.Literal['dublado', 'original']
+    ):
+    # get language id for prefered audio
+    match preferred_audio:
+        case 'dublado':
+            lang = '2'
+        
+        case 'original':
+            lang = '1'
+    
+    # turn single episode into list
+    if isinstance(episodes, int):
+        episodes = [episodes]
+
+    # get info about the serie
+    serie_info = serie(imdb)
+
+    # get selected season
+    for season_info in serie_info['seasons'].values():
+        if season_info['name'] == str(season):
+            break
+    
+    # download selected episode
+    for episode_info in season_info['episodes'].values():
+        if episodes == 'all':
+            ep_name = f'{episode_info['name']} - {episode_info['titlePt']}'
+            id = episode_info['id']
+            download_episode(ep_name, imdb, id, lang)
+
+        elif int(episode_info['name']) in episodes:
+            ep_name = f'{episode_info['name']} - {episode_info['titlePt']}'
+            id = episode_info['id']
+            download_episode(ep_name, imdb, id, lang)
+    
+
+# TODO: warn if prefered audio is unavaliable
+def download_filme(imdb: str, preferred_audio: typing.Literal['dublado', 'original']):
+    filme_info = filme(imdb)
+
+    audios = get_audios(imdb, filme_info['id'], 'filme')
+    for audio in audios:
+        if audio['audio'] == preferred_audio:
+            break
+
+    if 'warezcdn' in audio['servers']:
+        server = 'warezcdn'
+    else:
+        server = 'mixdrop'
+    
+    if server == 'warezcdn':
+        parts_url = get_parts_url(imdb, audio['id'], server, audio['audio'], 'serie')
+        download_from_m3u8(parts_url, f'{filme_info['title']}.mp4')
+    
+    else:
+        msg = "O episódio possui apenas o servidor 'mixdrop', que ainda não é suportado."
+        raise Exception(msg)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(dest='action', help='comandos disponíveis:')
+
+    # search parser
+    search_parser = subparser.add_parser('search', help='faz uma busca na API.')
+    search_parser.add_argument('search', nargs=1, type=str, help='o termo que será buscado.')
+
+
+    # info parser 
+    info_parser = subparser.add_parser('info', help='coleta informações sobre determinado filme ou série.')
+    info_subparser = info_parser.add_subparsers(dest='variant')
+
+    # filme parser
+    info_filme_parser = info_subparser.add_parser('filme', help='coleta informações de um filme.')
+    info_filme_parser.add_argument(
+        '-imdb', '--imdb', 
+        type=str, required=True, 
+        help='id do filme no imdb.'
+    )
+
+    # serie parser
+    info_serie_parser = info_subparser.add_parser('serie', help='cole informações de uma série.')
+    info_serie_parser.add_argument(
+        '-imdb', '--imdb', 
+        type=str, required=True, 
+        help='id da série filme no imdb.'
+    )
+
+
+    # download parser
+    download_parser = subparser.add_parser('download', help='coleta informações sobre determinado filme ou série.')
+    download_subparser = download_parser.add_subparsers(dest='variant')
+
+    # filme parser
+    download_filme_parser = download_subparser.add_parser('filme', help='coleta informações de um filme.')
+    download_filme_parser.add_argument(
+        '-imdb', '--imdb', 
+        type=str, required=True, 
+        help='id do filme no imdb.'
+    )
+    download_filme_parser.add_argument(
+        '-a', '--audio', 
+        choices=['dublado', 'original'], required=True, 
+        help='preferência de áudio (dublado ou original).'
+    )
+
+    # serie parser
+    download_serie_parser = download_subparser.add_parser('serie', help='cole informações de uma série.')
+    download_serie_parser.add_argument(
+        '-imdb', '--imdb', 
+        type=str, required=True, 
+        help='id da série filme no imdb.'
+    )
+    download_serie_parser.add_argument(
+        '-t', '--temporada', 
+        type=int, required=True,
+        help='número da temporada.'
+    )
+    download_serie_parser.add_argument(
+        '-e', '--episodios', 
+        type=int, required=True, nargs='+',
+        help='número dos episódios que serão baixados (-1 = todos).'
+    )
+    download_serie_parser.add_argument(
+        '-a', '--audio', 
+        choices=['dublado', 'original'], required=True, 
+        help='preferência de áudio (dublado ou original).'
+    )
+
+
+    args = parser.parse_args()
+    
+    match args.action:
+        case 'search':
+            print(json.dumps(search(args.search), indent=2))
+        
+        case 'info':
+            match args.variant:
+                case 'filme':
+                    print(json.dumps(filme(args.imdb), indent=2))
+                
+                case 'serie':
+                    print(json.dumps(serie(args.imdb), indent=2))
+                
+                case _:
+                    parser.print_help()
+                    exit()
+                
+        case 'download':
+            match args.variant:
+                case 'filme':
+                    download_filme(args.imdb, args.audio)
+                
+                case 'serie':
+                    if args.episodios[0] == -1:
+                        args.episodios == 'all'
+
+                    download_serie(args.imdb, args.temporada, args.episodios, args.audio)
+                
+                case _:
+                    parser.print_help()
+                    exit()
+        
+        case _:
+            parser.print_help()
